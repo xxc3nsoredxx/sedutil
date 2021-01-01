f8424ef4d8addba313c940afc7c53f98c8375893
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-01-01 13:35:05 -0600

    Stop tracking the buildroot config in temp dir

--- buildroot/prepare_buildroot.sh
+++ buildroot/prepare_buildroot.sh
@@ -4,14 +4,27 @@ VERSION='2020.11.1'
 NAME="buildroot-$VERSION"
 DOWNLOAD="$NAME.tar.gz"
 
+# Download buildroot
+rm buildroot-*
+rm -rf buildroot
 curl "https://git.buildroot.net/buildroot/snapshot/$DOWNLOAD" -O
 tar xf $DOWNLOAD
-rm -rf buildroot
 mv $NAME buildroot
 
-echo '*' > buildroot/.gitignore
-echo '!.config' >> buildroot/.gitignore
-
 cd buildroot
+
+# add the current buildroot packages
+sed -i '/sedutil/d' package/Config.in
+sed -i '/menu "System tools"/a \\tsource "package/sedutil/Config.in"' package/Config.in
+rm -rf package/sedutil
+cp -r ../../images/buildroot/packages/sedutil/ package/
+
+# add the old base kernel config
+mkdir 64bit
+cp ../../images/buildroot/64bit/kernel.config 64bit/
+
+# add the overlay
+cp -r ../../images/buildroot/64bit/overlay/ 64bit/
+
 make menuconfig
-cd ..
+make linux-menuconfig
