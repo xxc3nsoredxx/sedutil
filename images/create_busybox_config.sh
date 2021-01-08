202448efd1815df58c4a95f6bb180a0d2085a374
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-01-08 02:37:15 -0600

    Script to automate creating/copying Busybox config

---
+++ images/create_busybox_config.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cd scratch/buildroot
+echo 'Create busybox config ...'
+make O=64bit busybox-menuconfig
+
+cd 64bit
+echo 'Copying busybox config to 64bit/busybox.config ...'
+cp build/busybox*/.config busybox.config
+
+echo 'Copying 64bit/busybox.config to temp storage ...'
+cp busybox.config ../../../
