b5146020358db31ea08d72f348a7afe0b01f157b
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-01-13 00:57:04 -0600

    Create minimally sized images

--- images/build_UEFI64.sh
+++ images/build_UEFI64.sh
@@ -72,9 +72,24 @@ echo 'Remastering done!'
 rm -rf $BUILDTYPE
 mkdir $BUILDTYPE
 pushd $BUILDTYPE &> /dev/null
+    # Create system directory structure
+    echo 'Creating system directory structure ...'
+    mkdir -p system/EFI/boot
+    cp -v ../scratch/$SYSLINUX_VER/efi64/efi/syslinux.efi system/EFI/boot/bootx64.efi
+    cp -v ../scratch/$SYSLINUX_VER/efi64/com32/elflink/ldlinux/ldlinux.e64 system/EFI/boot/
+    cp -v ../scratch/buildroot/64bit/images/bzImage system/EFI/boot/
+    cp -v ../scratch/buildroot/64bit/images/rootfs.cpio.xz system/EFI/boot/
+    cp -v ../buildroot/syslinux.cfg system/EFI/boot/
+
+    # Calculate the total file size in 512B blocks
+    SIZE=$(du -d 0 -B 512 system | cut -f 1)
+    # Add space for the FAT structures
+    SIZE=$((SIZE + 50))
+
     # Create image file and loopback device
     echo 'Creating boot image ...'
-    dd if=/dev/zero of=$BUILDIMG bs=1M count=32
+    # +2048 to create the 1MiB padding
+    dd if=/dev/zero of=$BUILDIMG count=$((SIZE + 2048))
     sfdisk $BUILDIMG < ../layout.sfdisk
     LOOPDEV=$(losetup --show -f -o 1048576 $BUILDIMG)
     mkfs.vfat $LOOPDEV -n $BUILDTYPE
@@ -84,19 +99,16 @@ pushd $BUILDTYPE &> /dev/null
     mount $LOOPDEV image
     chmod 644 image
 
-    # Copy the system onto the image
-    echo 'Copying system to boot image ...'
-    mkdir -p image/EFI/boot
-    cp ../scratch/$SYSLINUX_VER/efi64/efi/syslinux.efi image/EFI/boot/bootx64.efi
-    cp ../scratch/$SYSLINUX_VER/efi64/com32/elflink/ldlinux/ldlinux.e64 image/EFI/boot/
-    cp ../scratch/buildroot/64bit/images/bzImage image/EFI/boot/
-    cp ../scratch/buildroot/64bit/images/rootfs.cpio.xz image/EFI/boot/
-    cp ../buildroot/syslinux.cfg image/EFI/boot/
+    # Transfer the system onto the image
+    echo 'Transfering system to boot image ...'
+    mv -v system/EFI image
 
     # Clean up
     umount image
     rmdir image
     losetup -d $LOOPDEV
+    rmdir system
+
     echo 'Compressing boot image ...'
     xz -9e $BUILDIMG
 popd &> /dev/null
