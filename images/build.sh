c31e1f7d912e4fdce0cb40b59867d31b5b79bcb6
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-01-21 12:01:50 -0600

    build.sh doesn't clean
    prepare.sh already does that

--- images/build.sh
+++ images/build.sh
@@ -3,8 +3,6 @@
 
 # Build the images
 pushd scratch/buildroot &> /dev/null
-    echo 'Cleaning ...' | tee output/build_output.txt
-    make clean |& tee -a output/build_output.txt
     echo 'Building the base system ...' >> output/build_output.txt
     make |& tee -a output/build_output.txt
 popd &> /dev/null
