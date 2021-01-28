417aa29e5bcfec07fd0fa1b8b222a9259e4e0824
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-01-28 17:45:19 -0600

    Test available disk space in dependency checker

--- images/check_deps.sh
+++ images/check_deps.sh
@@ -7,18 +7,26 @@ DEPS=(['curl']='curl' ['tar']='tar' ['gzip']='gzip' ['autoreconf']='autoconf' \
       ['gcc']='gcc' ['g++']='g++' ['patch']='patch' ['bzip2']='bzip2' \
       ['perl']='perl' ['cpio']='cpio' ['unzip']='unzip' ['rsync']='rsync' \
       ['file']='file' ['bc']='bc' ['wget']='wget')
-# Exit status - number of missing dependencies
+# Exit status - number of missing dependencies (incl. disk space)
 STATUS=0
 
 # Arg 1: missing package
 function warn {
-    echo "Missing dependency: $1"
+    echo "Missing dependency: '$1'!"
     STATUS=$((STATUS + 1))
 }
 
+# Check for missing packages
 for D in ${!DEPS[@]}; do
     which $D &> /dev/null || warn "${DEPS[$D]}"
 done
+
+# Check disk space
+SPACE="$(df -B1 --output=avail . | tail -n 1)"
+if [ $SPACE -lt 7516192768 ]; then
+    echo "Less than 7 GiB of space available on '$(findmnt -T . -o source -n)'"
+    STATUS=$((STATUS + 1))
+fi
 
 if [ $STATUS -eq 0 ]; then
     echo "No missing dependencies"
