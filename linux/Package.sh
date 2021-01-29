0e33bac37ff1ac0e6b568afb779775c05067288d
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-01-29 00:51:48 -0600

    Remove some unneeded files

--- linux/Package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm sedutil_LINUX.tgz
-strip --strip-debug ./CLI/dist/Release_i686/GNU-Linux/sedutil-cli
-strip --strip-unneeded ./CLI/dist/Release_i686/GNU-Linux/sedutil-cli
-strip --strip-debug ./CLI/dist/Release_x86_64/GNU-Linux/sedutil-cli
-strip --strip-unneeded ./CLI/dist/Release_x86_64/GNU-Linux/sedutil-cli
-tar --xform 's,^,sedutil/,' -czf sedutil_LINUX.tgz *.txt TestSuite.sh ./*.txt ../docs/* -C ./CLI/dist Release_i686/GNU-Linux/sedutil-cli Release_x86_64/GNU-Linux/sedutil-cli
